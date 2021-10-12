47b3eff6f15d15f196701cbcb1ab6e558492001f
Caitlyn Chau <caitlyn.chau@ibm.com>
2021-10-11 21:49:59 -0700

    added comment

--- client/src/Navigation.jsx
+++ client/src/Navigation.jsx
@@ -1,30 +1,40 @@
-import React from 'react';
-import {AppBar, Box, Button, Toolbar, IconButton, Typography } from "@material-ui/core";
+import React from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  Toolbar,
+  IconButton,
+  Typography,
+} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 
-
+/**
+ * Navigation bar
+ * @returns {node}
+ */
 function Navigation() {
-    return (
-        <Box sx={{flexGrow:1}}>
-            <AppBar position="static">
-                <Toolbar variant="dense">
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Briefcase
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    )
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Briefcase
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
-export default  Navigation;
+export default Navigation;
