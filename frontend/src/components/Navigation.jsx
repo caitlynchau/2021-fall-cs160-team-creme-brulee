fcd33a8c1605ab25f4889d6780a45e39d82e87e4
Caitlyn Chau <caitlynchau2@gmail.com>
2021-10-31 23:58:53 -0700

    remove unused imports

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { AppBar, Box, Button, Toolbar, IconButton, Typography } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
 
 /**
  * Navigation bar
@@ -8,24 +6,6 @@ import MenuIcon from '@material-ui/icons/Menu';
  */
 function Navigation() {
   
-    // <Box sx={{ flexGrow: 1 }}>
-    //   <AppBar className="app-bar" position="static">
-    //     <Toolbar variant="dense">
-    //       <IconButton size="medium" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-    //         <MenuIcon />
-    //       </IconButton>
-    //       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-    //         Briefcase
-    //       </Typography>
-
-    //       <Button color="inherit">Locations</Button>
-    //       <Button color="inherit">Food</Button>
-    //       <Button color="inherit">Inspiration</Button>
-    //       <Box display="flex" flexGrow={1} />
-    //       <Button color="inherit">Login</Button>
-    //     </Toolbar>
-    //   </AppBar>
-    // </Box>
     return (
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
