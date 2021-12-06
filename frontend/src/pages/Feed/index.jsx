3d94a4ec459e394e2a6ab2ada64a9e8252aece93
Caitlyn Chau <caitlynchau2@gmail.com>
2021-12-05 23:57:12 -0800

    feed & navbar styling

--- frontend/src/pages/Feed/index.jsx
+++ frontend/src/pages/Feed/index.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from 'react';
 import apis from "../../api";
+import Box from '@material-ui/core/Box';
 import Post from '../../components/Post';
+import { useSelector } from 'react-redux';
 
 
 function Feed() {
   const [isMounted, setIsMounted] = React.useState(false);
   const [posts, setPosts] = React.useState(null);
+
+  const currentUser = useSelector(state => state.user.currentUser);
 
   useEffect(() => {
     if (!isMounted) {
@@ -18,6 +22,9 @@ function Feed() {
   
   return (
     <div className="feed">
+      {currentUser !== '' &&
+        <Box className="feed--username">{`Welcome back, @${currentUser}`}</Box>
+      }
       {posts && posts.success && 
         posts.data.slice().reverse().map(post => 
           <Post key={post._id} postData={post} />
