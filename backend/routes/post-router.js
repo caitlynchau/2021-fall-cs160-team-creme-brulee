77c67da404971bc9e79cceeddaac72a7c58679a0
Caitlyn Chau <caitlynchau2@gmail.com>
2021-12-06 19:35:27 -0800

    backend: code formatting

--- backend/routes/post-router.js
+++ backend/routes/post-router.js
@@ -6,13 +6,13 @@ const PostCtrl = require('../controllers/post-ctrl');
 const router = express.Router();
 
 const storage  = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './public/')
-  },
-  filename: (req, file, cb) => {
-    const fileName = file.originalname.toLowerCase().split(' ').join('-');
-    cb(null, Date.now() + '-' + fileName);
-  }
+    destination: (req, file, cb) => {
+        cb(null, './public/')
+    },
+    filename: (req, file, cb) => {
+        const fileName = file.originalname.toLowerCase().split(' ').join('-');
+        cb(null, Date.now() + '-' + fileName);
+    }
 });
 const upload = multer({ storage: storage });
 
